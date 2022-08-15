81cec5290776a8b393aa5defc9a8fa7081327484
boski <boskitechh@gmail.com>
2022-08-15 23:09:21 +0100

    Initial Push

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="bg-blue w-screen h-screen">
+        <div className='text-4xl text-blue'>
+            Hello World
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'white': '#fff',
+      'sub-white':'#D9D9D9',
+      'green': '#06BC5F',
+      'activeclass': '#06BC5F',
+      'blue': '#25C8EB',
+      'yello': '#F0B90B',
+      'greentext': '#0E9E55',
+      'grey': '#D9D9D9',
+      'dark':'#1C1A1A',
+      'sub-dark': '#222020',
+      'navbar':'#030202'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
