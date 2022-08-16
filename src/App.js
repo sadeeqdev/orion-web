51a144a80b97944f4e6baa1189e39d71e17197c1
boski <boskitechh@gmail.com>
2022-08-16 17:50:09 +0100

    Added side bar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollTop";
 import Home from "./pages/home/Home";
-import Navbar from './components/navabar/Navbar';
-import Footer from './components/footer/Footer';
+// import Navbar from './components/navabar/Navbar';
+// import Footer from './components/footer/Footer';
 import TokenClaim from './pages/tokenClaim/TokenClaim';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
-   <BrowserRouter>
+    <BrowserRouter>
       <ScrollToTop>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/home" element={<Home />} />
           <Route path="/tokenclaim" element={<TokenClaim />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-        <Footer/>
+        {/* <Footer/> */}
       </ScrollToTop>
     </BrowserRouter>
   );
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import SideBar from "../../components/sidebar/SideBar"
+
+const Dashboard = () => {
+  return (
+    <div>
+        <SideBar/>
+    </div>
+  )
+}
+
+export default Dashboard
