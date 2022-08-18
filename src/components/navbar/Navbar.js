0974e617ca2f990b962cd8f77004d3f7580c6d01
boski <boskitechh@gmail.com>
2022-08-18 13:01:57 +0100

    Fixed sidebar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import orionLogo from '../../assets/img/Orion.svg'
+import orionLogo from '../../assets/img/Orion-logo.png'
 import Menu from '../../assets/img/Menu-2.png'
 
 const Navbar = () => {
@@ -34,8 +34,8 @@ const Navbar = () => {
     <div className='bg-navbar'>
         <div className='fixed w-72 -ml-72 bg-sub-dark h-screen transition-all' style={{zIndex:999, transform:(mobile && show)  && 'translateX(288px)', backdropFilter: 'blur(15px)'}}>
             <div className=' text-white pt-12 pb-7 w-11/12  lg:pt-28 lg:py-7 lg:w-5/6 2xl:w-4/6 mx-auto grid grid-cols-1 place-items-center justify-between'>
-                <div>
-                    <img src={orionLogo} alt="Logo"/>
+                <div className='flex justify-center'>
+                    <img src={orionLogo} width="50%" alt="Logo"/>
                 </div>
                 <div className='font-medium grid text-xl gap-y-5 mt-9 text-center' onClick={() => setShow(false)}>
                     <NavLink to="/home" className="hover:text-greentext" >Home</NavLink>
@@ -52,7 +52,7 @@ const Navbar = () => {
         </div>
         <div className=' text-white pt-12 pb-7 w-11/12  lg:pt-28 lg:py-7 lg:w-5/6 2xl:w-4/6 mx-auto flex justify-between'>
             <div>
-                <img src={orionLogo} alt="Logo"/>
+                <img src={orionLogo} width="35%" alt="Logo"/>
             </div>
             <div className='space-x-8 font-medium hidden lg:flex ' >
                 <NavLink to="/home" className="hover:text-greentext" >Home</NavLink>
